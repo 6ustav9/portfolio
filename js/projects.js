a39fc87d5ee35a3ef4461f43bd541695987641d9
6ustav9 <gustavoguedes95.gp15@gmail.com>
2022-07-28 12:09:05 -0300

    Adicionando caminho do projeto, seção Projetos

--- js/projects.js
+++ js/projects.js
@@ -4,18 +4,22 @@ let listProjects = [
     {
         link: "https://lyrics-redirect.vercel.app",
         name: "Lyrics Redirect",
+        githubProject: "https://github.com/6ustav9/lyrics-redirect"
     },
     {
         link: "https://recipesandcocktail.wordpress.com",
         name: "Recipes And Cocktails",
+        githubProject: "N/A"
     },
     {
         link: "https://moviesdbzn.herokuapp.com",
         name: "Movies DBZN",
+        githubProject: "https://github.com/6ust/heroku-movies-dbzn-react-version"
     }
     // ,{
     //     link: "",
     //     name: "Silent Hill Fans",
+    //     githubProject: ""
     // }     
 ];
 
@@ -24,30 +28,46 @@ let listProjects = [
 // Projects 
 for (let idxListProjects = 0; idxListProjects < listProjects.length; idxListProjects++) {
 	let divElement = document.createElement('div');
-    let linkElement = document.createElement('a');
-    let titleImageElement = document.createElement('h2');
+    let linkSiteElement = document.createElement('a');
+    let linkGithubProjectElement = document.createElement('a');
+    let titleSiteElement = document.createElement('h2');
+    let titleGithubProjectElement = document.createElement('h2');
     let imgProjectsItemlistElement = document.createElement('iframe');
-    let txtTitle = document.createTextNode(listProjects[idxListProjects].name);
-    let txtlinkUrl = document.createTextNode(listProjects[idxListProjects].link);
+    let txtTitleSite = document.createTextNode(listProjects[idxListProjects].name);
+    let txtTitleGithubProject = document.createTextNode(listProjects[idxListProjects].githubProject);
     
     imgProjectsItemlistElement.setAttribute("src", listProjects[idxListProjects].link);
     imgProjectsItemlistElement.setAttribute("scrolling", "no");
-    linkElement.setAttribute("href", listProjects[idxListProjects].link);
-    linkElement.setAttribute("target", "_blank");
+
+    // link Site
+    linkSiteElement.setAttribute("href", listProjects[idxListProjects].link);
+    linkSiteElement.setAttribute("target", "_blank");
+    // link Github Project
+    linkGithubProjectElement.setAttribute("href", listProjects[idxListProjects].link);
+    linkGithubProjectElement.setAttribute("target", "_blank");
 
 
 	divElement.classList.add("projects__container__image__item")
     imgProjectsItemlistElement.classList.add("projects__container__image__item__iframe");
-    titleImageElement.classList.add('projects__container__image__item__link__title');
-    linkElement.classList.add('projects__container__image__item__link');
+    // Classes Site
+    titleSiteElement.classList.add('projects__container__image__item__link-site__title');
+    linkSiteElement.classList.add('projects__container__image__item__link-site');
+    // Classes Github Project
+    titleGithubProjectElement.classList.add('projects__container__image__item__link-github-project__title');
+    linkGithubProjectElement.classList.add('projects__container__image__item__link-github-project');
 
-    titleImageElement.appendChild(imgProjectsItemlistElement);
-    titleImageElement.appendChild(txtTitle);
 
-    linkElement.appendChild(titleImageElement);
+    titleSiteElement.appendChild(imgProjectsItemlistElement);
+    titleSiteElement.appendChild(txtTitleSite);
+
+    titleGithubProjectElement.appendChild(txtTitleGithubProject);
+
+    linkSiteElement.appendChild(titleSiteElement);
+    linkGithubProjectElement.appendChild(titleGithubProjectElement);
     
     divElement.appendChild(imgProjectsItemlistElement);
-    divElement.appendChild(linkElement);
+    divElement.appendChild(linkSiteElement);
+    divElement.appendChild(linkGithubProjectElement);
 
 	containerProjects.appendChild(divElement);
 }
