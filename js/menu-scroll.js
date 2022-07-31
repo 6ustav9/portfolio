1953a65a4b07b8559ac8a681bfbe9732c53b3cf6
6ustav9 <gustavoguedes95.gp15@gmail.com>
2022-07-31 20:46:28 -0300

    Ativação do menu link de acordo com o Scroll

--- js/menu-scroll.js
+++ js/menu-scroll.js
@@ -4,4 +4,38 @@ document.addEventListener('scroll' , (event) => {
     } else {
         document.getElementById("menu").classList.remove("menu-sroll")
     }
+    
+    if(Math.round(window.pageYOffset) >= document.getElementById("home").offsetTop 
+        && Math.round(window.pageYOffset) < (document.getElementById("home").offsetTop + document.getElementById("home").offsetHeight)) {
+            scrollActiveMenu(0); 
+    } else if(Math.round(window.pageYOffset) >= document.getElementById("presentation").offsetTop
+        && Math.round(window.pageYOffset) < (document.getElementById("presentation").offsetTop + document.getElementById("presentation").offsetHeight)) {
+            scrollActiveMenu(1); 
+    } else if(Math.round(window.pageYOffset) >= document.getElementById("skills").offsetTop
+        && Math.round(window.pageYOffset) < (document.getElementById("skills").offsetTop + document.getElementById("skills").offsetHeight)) {
+            scrollActiveMenu(2); 
+    } else if(Math.round(window.pageYOffset) >= document.getElementById("animations").offsetTop
+        && Math.round(window.pageYOffset) < (document.getElementById("animations").offsetTop + document.getElementById("animations").offsetHeight)) {
+            scrollActiveMenu(3); 
+    } else if(Math.round(window.pageYOffset) >= document.getElementById("projects").offsetTop
+        && Math.round(window.pageYOffset) < (document.getElementById("projects").offsetTop + document.getElementById("projects").offsetHeight)) {
+            scrollActiveMenu(4); 
+        } else if(Math.round(window.pageYOffset) >= document.getElementById("screenshots").offsetTop
+            && Math.round(window.pageYOffset) < (document.getElementById("screenshots").offsetTop + document.getElementById("screenshots").offsetHeight)) {
+            scrollActiveMenu(5); 
+    } else {
+
+    }
+
 })
+
+function scrollActiveMenu(optionActive) {
+    for (let i = 0; i < document.getElementsByClassName('menu__list__item').length; i++) {        
+        if(i == optionActive) {
+            document.getElementsByClassName('menu__list__item')[i].getElementsByClassName('menu__list__item__link')[0].classList.add("active");
+        } else {
+            document.getElementsByClassName('menu__list__item')[i].getElementsByClassName('menu__list__item__link')[0].classList.remove("active");
+        }
+    }
+
+}
